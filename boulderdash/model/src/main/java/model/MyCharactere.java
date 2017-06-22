95e098ce78ca7c81b49778218fadc2339d32c1cf
DylanBC <dylan----2011@hotmail.fr>
2017-06-22 16:29:31 +0200

    Mise à jours de charac pour apporter les différentes actions

--- boulderdash/model/src/main/java/model/MyCharactere.java
+++ boulderdash/model/src/main/java/model/MyCharactere.java
@@ -1,5 +1,65 @@
 package model;
 
 public class MyCharactere {
+	  /** The Constant SPRITE. */
+    private static final Sprite sprite          = new Sprite('c', "$_35.jpg");
+
+    /** The Constant spriteTurnLeft. */
+    private static final Sprite spriteTurnLeft  = new Sprite('c', "MyVehicleLeft.png");
+
+    /** The Constant spriteTurnRight. */
+    private static final Sprite spriteTurnRight = new Sprite('c', "MyVehicleRight.png");
+
+    /** The Constant spriteExplode. */
+    private static final Sprite spriteExplode   = new Sprite('c', "MyVehicleExplode.png");
+    
+    private static final Sprite spriteUp = new Sprite ('c',"MyVehicleExplode.png");
+    public MyVehicle(final int x, final int y, final IMap map) throws IOException {
+        super(x, y, sprite, road, Permeability.BLOCKING);
+        spriteTurnLeft.loadImage();
+        spriteTurnRight.loadImage();
+        spriteExplode.loadImage();
+    }
+    /*
+     * (non-Javadoc)
+     * @see fr.exia.insanevehicles.model.element.mobile.Mobile#moveLeft()
+     */
+    @Override
+    public final void moveLeft() {
+        super.moveLeft();
+        this.setSprite(spriteTurnLeft);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see fr.exia.insanevehicles.model.element.mobile.Mobile#moveRight()
+     */
+    @Override
+    public final void moveRight() {
+        super.moveRight();
+        this.setSprite(spriteTurnRight);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see fr.exia.insanevehicles.model.element.mobile.Mobile#die()
+     */
+    @Override
+    protected final void die() {
+        super.die();
+        this.setSprite(spriteExplode);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see fr.exia.insanevehicles.model.element.mobile.Mobile#doNothing()
+     */
+    @Override
+    public final void doNothing() {
+        super.doNothing();
+        this.setSprite(sprite);
+    }
+}
+
 
 }
