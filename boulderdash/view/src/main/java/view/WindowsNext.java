5df5a7ee6e0b655968a0cd29c64c401762e879d1
DylanBC <dylan----2011@hotmail.fr>
2017-06-24 00:01:35 +0200

    Voici la fenêtre qui va contenir la map

--- boulderdash/view/src/main/java/view/WindowsNext.java
+++ boulderdash/view/src/main/java/view/WindowsNext.java
@@ -2,16 +2,31 @@ package view;
 
 import javax.swing.JFrame;
 
-public class WindowsNext {
-  public static void main(String[] args){
+public class WindowsNext  extends Storages {
+  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
-    JFrame fenetre = new JFrame();
+public static void main(String[] args){
+	  /**
+	   * this is windows
+	   */
+    JFrame windows = new JFrame();
                 
-    fenetre.setTitle("Mort connard regarde");
-    fenetre.setSize(500, 500);
-    fenetre.setLocationRelativeTo(null);
-    fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    fenetre.setContentPane(new Storage());
-    fenetre.setVisible(true);
-  }       
+    windows.setTitle(" regarde");
+    windows.setSize(500, 500);
+    windows.setLocationRelativeTo(null);
+    windows.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    windows.setContentPane(new Storages());
+    
+   
+    windows.setVisible(true);
+  }
+  
+
+   
+       
 }
+
+  	
